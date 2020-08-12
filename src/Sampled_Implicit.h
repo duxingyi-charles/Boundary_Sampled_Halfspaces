6d837a4853bb35db79f1cf59289ab566d866d17e
CharlesDu <dohoney@126.com>
2020-08-12 18:29:50 -0500

    use Eigen

--- src/Sampled_Implicit.h
+++ src/Sampled_Implicit.h
@@ -2,27 +2,26 @@
 #define SAMPLED_IMPLICIT_H
 
 #include <vector>
-#include "Vec3.h"
+#include <eigen3/Eigen/Core>
 
-typedef Vec3 Point;
+
+typedef Eigen::Vector3d Point;
 
 class Sampled_Implicit
 {
 public:
 	Sampled_Implicit() = default;
-	Sampled_Implicit(const std::vector<Point> &pts)
+	explicit Sampled_Implicit(const std::vector<Point> &pts)
 	: sample_points(pts) {};
 
 	virtual ~Sampled_Implicit() = default;
 
 	std::vector<Point> get_sample_points() const { return sample_points; }
 
-	virtual double function_at(const Point &p) const = 0;
-	virtual Vec3 gradient_at(const Point &p) const = 0;
+	virtual double function_at(const Point &) const = 0;
+	virtual Eigen::Vector3d gradient_at(const Point &) const = 0;
 
 protected:
-
-	// void set_sample_points(const std::vector<Point> &pts) { sample_points = pts; } 
 
 	std::vector<Point> sample_points;
 
