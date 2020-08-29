86bd4fbb8dbcc1cc13e7849bf589729bce9256b5
CharlesDu <dohoney@126.com>
2020-08-29 15:54:24 -0500

    organize different tests into separate functions

--- main.cpp
+++ main.cpp
@@ -6,26 +6,27 @@
 
 #include "src/Grid.h"
 
-int main()
-{
-    // ------- sampled implicits -------------
-//    Hermite_RBF_sImplicit rbf;
-//    // import
-//    std::string pts_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input.xyz";
-//    std::string coeff_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input_rbfCoeff";
-//    rbf.import_Hermite_RBF(pts_file,coeff_file);
-//
-//    // test
-//    Point p(0.5,1,-2);
-//    double f = rbf.function_at(p);
-//    std::cout << "f(" << p(0) << "," << p(1) << "," << p(2) << ") = " << f << std::endl;
-//
-//    Eigen::Vector3d g = rbf.gradient_at(p);
-//    std::cout << "g(" << p(0) << "," << p(1) << "," << p(2) << ") = " << std::endl;
-//    std::cout << g << std::endl;
 
+// sampled implicits
+void test1() {
+    Hermite_RBF_sImplicit rbf;
+    // import
+    std::string pts_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input.xyz";
+    std::string coeff_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input_rbfCoeff";
+    rbf.import_Hermite_RBF(pts_file,coeff_file);
 
-    // -------- cell arrangement ----------
+    // test: compute function and gradient
+    Point p(0.5,1,-2);
+    double f = rbf.function_at(p);
+    std::cout << "f(" << p(0) << "," << p(1) << "," << p(2) << ") = " << f << std::endl;
+
+    Eigen::Vector3d g = rbf.gradient_at(p);
+    std::cout << "g(" << p(0) << "," << p(1) << "," << p(2) << ") = " << std::endl;
+    std::cout << g << std::endl;
+}
+
+// arrangement in a single-cell grid
+void test2() {
     Plane_sImplicit plane1(Point(0,-1,1), Point(-1,0,1), Point(1,-1,0));
     Plane_sImplicit plane2(Point(-1,-1,1), Point(1,1,-1), Point(-1,1,-1));
     Plane_sImplicit plane3(Point(0,0,0), Point(1,0,0), Point(0,1,0));
@@ -39,7 +40,11 @@ int main()
     grid.compute_arrangement(plane3);
     // after
     grid.export_grid("/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/result.grid");
+}
 
+int main()
+{
+    test2();
     return 0;
 }
 
