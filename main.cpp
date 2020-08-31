39fee94855c20e4c52389414b23d5b84783aa3d0
CharlesDu <dohoney@126.com>
2020-08-31 13:07:38 -0500

    test: arrangement in uniform grid (multiple implicits)

--- main.cpp
+++ main.cpp
@@ -84,9 +84,32 @@ void test5() {
     grid.export_grid("/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/result.grid");
 }
 
+// arrangement in uniform grid (multiple implicits)
+void test6() {
+    // import RBF
+    Hermite_RBF_sImplicit rbf;
+    std::string pts_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input.xyz";
+    std::string coeff_file = "/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/input_rbfCoeff";
+    rbf.import_Hermite_RBF(pts_file,coeff_file);
+
+    // plane
+    Plane_sImplicit plane1(Point(0,-1,1), Point(-1,0,1), Point(1,-1,0));
+
+    // init grid
+    Grid grid(Point(-2,-2,-2), Point(2,2,2), 15, 15, 15);
+    // before
+    grid.export_grid("/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/init.grid");
+    // compute arrangement
+    grid.compute_arrangement(rbf);
+    grid.compute_arrangement(plane1);
+    // after
+    grid.export_grid("/Users/charlesdu/Downloads/research/implicit_modeling/code/VIPSS/data/MMA_tmp/result.grid");
+}
+
+
 int main()
 {
-    test5();
+    test6();
     return 0;
 }
 
