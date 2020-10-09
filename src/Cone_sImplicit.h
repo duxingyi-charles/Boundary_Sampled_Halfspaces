7892e99a3ee3f7c928c8fc74f5de18bc668bb569
CharlesDu <dohoney@126.com>
2020-10-09 11:22:36 -0500

    Cone primitive class

---
+++ src/Cone_sImplicit.h
@@ -0,0 +1,57 @@
+//
+// Created by Charles Du on 10/9/20.
+//
+
+#ifndef PSI_CONE_SIMPLICIT_H
+#define PSI_CONE_SIMPLICIT_H
+
+#include "Sampled_Implicit.h"
+
+
+class Cone_sImplicit : public Sampled_Implicit
+{
+public:
+    // standard cone
+    Cone_sImplicit() : Sampled_Implicit(),
+    apex(0,0,0), axis_unit_vector(0,0,-1), apex_angle(M_PI_4), is_flipped(false)
+    {};
+
+    Cone_sImplicit(const Point &p, const Eigen::Vector3d &v, double a, bool flip) : Sampled_Implicit(),
+    apex(p), axis_unit_vector(v.normalized()), apex_angle(fmod(a,M_PI)), is_flipped(flip)
+    {};
+
+    ~Cone_sImplicit() override = default;
+
+    double function_at(const Point &x) const override;
+    Eigen::Vector3d   gradient_at(const Point &x) const override;
+
+private:
+    // p: cone apex
+    Point apex;
+    // v: unit vector along cone axis
+    Eigen::Vector3d axis_unit_vector;
+    // a: angle between generatrix and axis vector (range [0,pi))
+    double apex_angle;
+    // is_flipped = false: f(x) = cos(a) ||x-p|| - dot(x-p,v)
+    // is_flipped = true : f(x) = dot(x-p,v) - cos(a) ||x-p||
+    bool is_flipped;
+
+};
+
+double Cone_sImplicit::function_at(const Point &x) const {
+    if (is_flipped) {
+        return (x-apex).dot(axis_unit_vector) - cos(apex_angle) * (x-apex).norm();
+    } else {
+        return cos(apex_angle) * (x-apex).norm() - (x-apex).dot(axis_unit_vector);
+    }
+}
+
+Eigen::Vector3d Cone_sImplicit::gradient_at(const Point &x) const {
+    if (is_flipped) {
+        return axis_unit_vector - cos(apex_angle) * (x-apex).normalized();
+    } else {
+        return cos(apex_angle) * (x-apex).normalized() - axis_unit_vector;
+    }
+}
+
+#endif //PSI_CONE_SIMPLICIT_H
