f6b37ac64f280426dea4f8b0d9e0bc851e8f6a04
Qingnan Zhou <qzhou@adobe.com>
2020-11-05 21:10:46 -0800

    Update libigl.

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG v2.2.0
+    GIT_TAG 1e905cba3d5e7d82487a6826d0d3fa7cd47de114
 )
 FetchContent_GetProperties(libigl)
 if(libigl_POPULATED)
