db5ff47e9dac3568435dc3802b703aa5fa011994
CharlesDu <dohoney@126.com>
2020-11-25 13:40:32 -0600

    minor clear

--- apps/psi_cli.cpp
+++ apps/psi_cli.cpp
@@ -51,16 +51,6 @@ int main(int argc, char** argv) {
 
     psi->run(grid_spec, implicit_functions);
 
-    // add implicit
-    Point center(1,1,0);
-//    Point center(0,0,0);
-    double radius = 1.0;
-    std::vector<Point> samples;
-    samples.push_back(Point(1,0,0));
-    implicit_functions.push_back(std::make_unique<Sphere_sImplicit>(center, radius));
-    implicit_functions.back()->set_sample_points(samples);
-    psi->run(grid_spec, implicit_functions);
-
     // export result
     psi->export_data(args.output_grid_file);
 
