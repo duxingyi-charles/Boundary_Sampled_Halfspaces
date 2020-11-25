8bd2b39526a53baa3c22a15569f86c6e04e5d114
CharlesDu <dohoney@126.com>
2020-11-25 13:39:55 -0600

    add initializer Quadric_sImplicit

--- src/config.h
+++ src/config.h
@@ -12,6 +12,9 @@ std::unique_ptr<Sampled_Implicit> initialize_RBF(const nlohmann::json& entry,
 std::unique_ptr<Sampled_Implicit> initialize_Plane(
     const nlohmann::json& entry, const std::string& path_name);
 
+std::unique_ptr<Sampled_Implicit> initialize_Quadric(
+        const nlohmann::json& entry, const std::string& path_name);
+
 std::unique_ptr<Sampled_Implicit> initialize_Sphere(
     const nlohmann::json& entry, const std::string& path_name);
 
