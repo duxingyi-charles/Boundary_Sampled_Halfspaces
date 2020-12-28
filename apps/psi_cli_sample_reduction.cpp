ce89363da87bc546ee76025bef499f19757541a8
CharlesDu <dohoney@126.com>
2020-12-28 12:37:00 -0600

    read PSI parameters from param_file

--- apps/psi_cli_sample_reduction.cpp
+++ apps/psi_cli_sample_reduction.cpp
@@ -19,12 +19,14 @@ int main(int argc, char** argv) {
         std::string config_file;
         std::string output_grid_file;
         std::string config_file_sparse;
-        int grid_size;
+        std::string param_file;
         std::string arrangement_algorithm;
     } args;
 
     CLI::App app{"Piecewise implicit surface demo"};
     app.add_option("-G,--grid-file", args.grid_file, "Grid spec file")
+            ->required();
+    app.add_option("-P,--param-file", args.param_file, "Parameter spec file")
             ->required();
     app.add_option("-A,--arr-algo", args.arrangement_algorithm, "Arrangement algorithm " )
             ->required();
@@ -44,6 +46,8 @@ int main(int argc, char** argv) {
 
     auto grid_spec = GridSpec::parse_grid_spec(args.grid_file);
 
+    auto param_spec = PSI_Param::parse_psi_param(args.param_file);
+
     // PSI
     Topo_PSI topo_psi;
     Mesh_PSI mesh_psi;
@@ -54,7 +58,9 @@ int main(int argc, char** argv) {
     } else {
         psi = &mesh_psi;
     }
+    psi->set_parameters(param_spec);
 
+    // run PSI on dense samples
     psi->run(grid_spec, implicit_functions);
 
     // sparse samples
@@ -63,7 +69,7 @@ int main(int argc, char** argv) {
     //
     {
         ScopedTimer<> timer("sample reduction");
-//        psi->reduce_samples();
+//        psi->reduce_samples(nullptr);
         psi->reduce_samples(&implicit_functions_sparse);
     }
 
