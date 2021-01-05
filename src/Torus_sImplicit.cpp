1aa2aec92b80c27f8b5a719a4baeff744210ca9c
CharlesDu <dohoney@126.com>
2021-01-05 15:37:12 -0600

    use explicit type to prevent Eigen compiling errors

--- src/Torus_sImplicit.cpp
+++ src/Torus_sImplicit.cpp
@@ -7,9 +7,9 @@
 
 double Torus_sImplicit::function_at(const Point &x) const {
     double val;
-    auto vec = x - center;
-    auto vec_para = vec.dot(axis_unit_vector) * axis_unit_vector;
-    auto vec_perp = vec - vec_para;
+    Eigen::Vector3d vec = x - center;
+    Eigen::Vector3d vec_para = vec.dot(axis_unit_vector) * axis_unit_vector;
+    Eigen::Vector3d vec_perp = vec - vec_para;
     if (vec_perp.norm() == 0) { // point x lies on torus axis
         val = vec_para.squaredNorm() + major_radius * major_radius - minor_radius * minor_radius;
     } else {
@@ -27,9 +27,9 @@ double Torus_sImplicit::function_at(const Point &x) const {
 
 Eigen::Vector3d Torus_sImplicit::gradient_at(const Point &x) const {
     Eigen::Vector3d grad;
-    auto vec = x - center;
-    auto vec_para = vec.dot(axis_unit_vector) * axis_unit_vector;
-    auto vec_perp = vec - vec_para;
+    Eigen::Vector3d vec = x - center;
+    Eigen::Vector3d vec_para = vec.dot(axis_unit_vector) * axis_unit_vector;
+    Eigen::Vector3d vec_perp = vec - vec_para;
     if (vec_perp.norm() == 0) { // point x lies on torus axis
         grad = 2 * vec;
     } else {
