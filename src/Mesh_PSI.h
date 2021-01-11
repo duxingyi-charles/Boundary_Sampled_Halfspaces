6667a251006f1757323882c1e7e027b95f8b04b2
CharlesDu <dohoney@126.com>
2021-01-11 11:23:47 -0600

    mesh generators for primtives

--- src/Mesh_PSI.h
+++ src/Mesh_PSI.h
@@ -37,7 +37,25 @@ private:
             IGL_Mesh &merged_mesh,
             Eigen::VectorXi &face_to_mesh);
 
-    IGL_Mesh generate_plane(const GridSpec &grid, const Point &p, const Eigen::Vector3d &normal);
+    IGL_Mesh generate_plane(const GridSpec &grid,
+                            const Point &p, const Eigen::Vector3d &normal);
+
+    IGL_Mesh generate_cylinder(const GridSpec &grid, int n,
+                               const Point& axis_point, const Eigen::Vector3d &axis_unit_vector,
+                               double radius, bool is_flipped);
+
+    IGL_Mesh generate_cone(const GridSpec &grid, int n,
+                           const Point& apex, const Eigen::Vector3d& axis_unit_vector,
+                           double apex_angle, bool is_flipped);
+//
+//    IGL_Mesh generate_sphere(const GridSpec &grid, int num_subdivide,
+//                             const Point& center, double radius, bool is_flipped);
+//
+//    IGL_Mesh generate_torus(const GridSpec &grid, int n_major, int n_minor,
+//                            const Point& center, const Eigen::Vector3d& axis_unit_vector,
+//                            double major_radius, double minor_radius,
+//                            bool is_flipped);
+
     IGL_Mesh generate_random_plane(const GridSpec &grid);
 
 };
